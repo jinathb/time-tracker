b621b931b48ca4c887083799e70116c076ac2d96
Jinath Buddhika <jinathbuddhika@gmail.com>
2021-06-07 12:06:46 +0530

    added gobal style component

---
+++ src/components/GlobalStyles.js
@@ -0,0 +1,33 @@
+import { createStyles, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(() => createStyles({
+  '@global': {
+    
+    html: {
+      '-webkit-font-smoothing': 'antialiased',
+      '-moz-osx-font-smoothing': 'grayscale',
+      height: '100%',
+      width: '100%'
+    },
+    body: {
+      backgroundColor: '#f4f6f8',
+      height: '100%',
+      width: '100%'
+    },
+    a: {
+      textDecoration: 'none'
+    },
+    '#root': {
+      height: '100%',
+      width: '100%'
+    }
+  }
+}));
+
+const GlobalStyles = () => {
+  useStyles();
+
+  return null;
+};
+
+export default GlobalStyles;
